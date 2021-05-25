74e518fac7b2e9c8dd504ce5a3d5155e73c9d95f
Joshua Higginbotham <jhigginbotham64@gmail.com>
2021-05-24 22:00:55 -0500

    modifications to install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 wget -c https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz -O - | tar xz
 
 # get packages and set up IJulia
-echo -e "\n" | ./julia-1.6.1/bin/julia --project=@. -e "using Pkg; Pkg.update(); using IJulia; notebook()"
+echo -e "\n" | ./julia-1.6.1/bin/julia --project=@. -e "using Pkg; Pkg.update(); using IJulia; notebook(detached=true)"
+
+# kill notebook, not reachable by default so we need to start properly later anyway
+killall -9 jupyter-notebook
 
 # install chime_home dataset
-# wget -c https://archive.org/download/chime-home/chime_home.tar.gz -O | tar xz
+wget -c https://archive.org/download/chime-home/chime_home.tar.gz -O | tar xz
