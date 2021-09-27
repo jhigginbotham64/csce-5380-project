f7720ba0c20f9e59c578169607e6e124eb97205e
Joshua Higginbotham <jhigginbotham64@gmail.com>
2021-09-27 12:20:12 -0500

    update for new execution environment

--- run.sh
+++
@@ -1,9 +0,0 @@
-# run.sh
-
-# runs miniconda jupyter notebook on default port.
-# usually this is 8888, but if you're connecting
-# remotely then edit this command to make sure it
-# runs on the same port as your SSH tunnel. also,
-# if you're not able to connect then make sure you
-# get the right URL token from stdout.
-~/.julia/conda/3/bin/jupyter notebook --no-browser &
