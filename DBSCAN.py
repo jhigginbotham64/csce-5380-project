db596f1273adce1a39f22b47e39da2f8ba96fb24
Joshua Higginbotham <jhigginbotham64@pop-os.localdomain>
2022-01-20 10:29:08 -0600

    more DBSCAN skeleton

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,8 +1,30 @@
+import numpy as np
+
+def dist(a, b):
+  # euclidean distance
+  return np.linalg.norm(a-b)
+
+class SimpleDBSCANNode:
+  visited = None
+  core = None
+  pos = None
+  eps_nhood = None
+  cluster = None
+
+  def __init__(self, pos, eps_nhood, cluster = None, core = False, visited = False):
+    self.pos = pos
+    self.core = core
+    self.eps_nhood = eps_nhood
+    self.visited = visited
+    self.cluster = cluster
+
 class SimpleDBSCAN:
   eps = None
+  min_samples = None
 
-  def __init__(self, eps = None):
+  def __init__(self, eps = None, min_samples = 5):
     self.eps = eps
+    self.min_samples = min_samples
   
   def fit_predict(self, X):
     pass
